e1398a8b4d6a8742a84dc749b4bb7db78340ccd2
sadsadass99 <81268449+sadsadass99@users.noreply.github.com>
2021-04-17 17:20:46 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ var canvas = new fabric.Canvas('myCanvas');
 player_x = 10;
 player_y = 10;
 
-block_image_width = 1000;
-block_image_height = 1000;
+block_image_width = 500;
+block_image_height = 500;
 
 var player_object= "";
 var block_image_object= "";
